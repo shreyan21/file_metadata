55c5dc8b12cce4c644c5aef97eb78081ed0fcd74
Utkarsh Singh Rajawat <rajawatshrayansh@gmail.com>
2024-09-18 00:33:38 +0530

    Added

--- index.js
+++ index.js
@@ -1,16 +1,36 @@
 var express = require('express');
 var cors = require('cors');
+const multer = require('multer');
+const path = require('path');
 require('dotenv').config()
-
 var app = express();
 
 app.use(cors());
 app.use('/public', express.static(process.cwd() + '/public'));
+app.use(express.json())
+app.use(express.urlencoded({extended:true}))
+
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+      cb(null, 'uploads/images')
+  }, filename: (req, file, cb) => {
+    cb(null,file.fieldname+"_"+ Date.now() + '-' + path.extname(file.originalname))
+
+  }
+}
+)
+
+
+const upload=multer({storage})
 
 app.get('/', function (req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
+app.post('/api/fileanalyse',upload.single('upfile'),(req,res)=>{
+  res.json({name:req.file.originalname,type:req.file.mimetype,size:req.file.size})
+
+})
 
 
 
